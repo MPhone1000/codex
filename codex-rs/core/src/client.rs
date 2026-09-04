bdee7b1f0513b7269a42b981efbe24fc9b022977
MPhone1000 <25451973+MPhone1000@users.noreply.github.com>
2026-09-05 02:21:20 +0800

    feat: support internal model request overrides
    (cherry picked from commit 40bdc90301f75b495f560d6f0f8b8264f7ec67f4)
    (cherry picked from commit 412df50f7073d73253beaa7610db1aa27d2ade99)
    (cherry picked from commit 7db7a90e14603ad6b42385d167697032d6b1004d)
    (cherry picked from commit 0ab4a20cf46ad79316e7e16436c0a005ac5421db)
    (cherry picked from commit 32d5f6f4a9813a26ae279fdaf9c7016f6e7405de)
    (cherry picked from commit b8fd65e3b4ed98bb3db1712db918aa8343a06e85)
    (cherry picked from commit 8cf52aeb626627731a44ef4fe3016be9479c286e)
    (cherry picked from commit 45605aae105cdb0dc56390365e87363e05d8aaea)
    (cherry picked from commit 97ce0326a2647e0891e8d64fa13611a9f347571d)
    (cherry picked from commit e4c67c55cf5b4d77ea39a1599d6c1b71a6078e5a)
    (cherry picked from commit 168bca7d9b638f224c5453cf2a4542748ebdf23b)
    (cherry picked from commit faf4f988d962a6fc962713050afb699090bf5de6)
    (cherry picked from commit f8d5e9a4ede9bb9035c5c6ee496d858585b79153)
    (cherry picked from commit 6bff4bbc860a2c3a0e8e5d690c087b17773e4a20)

--- codex-rs/core/src/client.rs
+++ codex-rs/core/src/client.rs
@@ -173,6 +173,7 @@ const RESPONSES_COMPACT_ENDPOINT: &str = "/responses/compact";
 // period between stream events.
 const COMPACT_REQUEST_TIMEOUT_IDLE_MULTIPLIER: u32 = 4;
 const MEMORIES_SUMMARIZE_ENDPOINT: &str = "/memories/trace_summarize";
+
 #[cfg(test)]
 pub(crate) const WEBSOCKET_CONNECT_TIMEOUT: Duration =
     Duration::from_millis(DEFAULT_WEBSOCKET_CONNECT_TIMEOUT_MS);
@@ -209,6 +210,7 @@ struct ModelClientState {
     originator: String,
     model_verbosity: Option<VerbosityConfig>,
     content_item_kinds_enabled: bool,
+    model_max_output_tokens: Option<i64>,
     enable_request_compression: bool,
     include_timing_metrics: bool,
     beta_features_header: Option<String>,
@@ -444,6 +446,7 @@ impl ModelClient {
         originator: String,
         model_verbosity: Option<VerbosityConfig>,
         content_item_kinds_enabled: bool,
+        model_max_output_tokens: Option<i64>,
         enable_request_compression: bool,
         include_timing_metrics: bool,
         beta_features_header: Option<String>,
@@ -468,6 +471,7 @@ impl ModelClient {
                 originator,
                 model_verbosity,
                 content_item_kinds_enabled,
+                model_max_output_tokens,
                 enable_request_compression,
                 include_timing_metrics,
                 beta_features_header,
@@ -622,6 +626,7 @@ impl ModelClient {
             service_tier,
             prompt_cache_key,
             text,
+            max_output_tokens,
             ..
         } = request;
         self.prepare_response_items_for_request(&mut input);
@@ -639,6 +644,7 @@ impl ModelClient {
                 client_setup.auth.as_ref(),
                 prompt.cyber_access_program,
             ),
+            max_output_tokens,
         };
 
         let mut extra_headers = ApiHeaderMap::new();
@@ -992,6 +998,7 @@ impl ModelClient {
             text,
             client_metadata: Some(responses_metadata.client_metadata()),
             access_programs: None,
+            max_output_tokens: self.state.model_max_output_tokens,
         };
         Ok(request)
     }
