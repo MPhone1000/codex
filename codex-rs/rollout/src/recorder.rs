ec39031f7a60e0cacae1a1f11d432fa008a7ec85
MPhone1000 <25451973+MPhone1000@users.noreply.github.com>
2026-09-05 02:20:28 +0800

    feat: preserve wire session ids
    (cherry picked from commit 4fc06d0deeb2e8bd68abcd4e9779f76aac151f65)
    (cherry picked from commit fc07fda4f5b085884bd5dcd4383a368a8bfe23b8)
    (cherry picked from commit b38906568df29af8c52af726feff8869d54563bf)
    (cherry picked from commit e51276a285e0b42ec58dd680ab711b5285243714)
    (cherry picked from commit b8f7e55345b1c45da09f575d1283d7b3e35565b3)
    (cherry picked from commit 46a55fbe95a59e160f86af44019e28bed0647353)
    (cherry picked from commit 39697681315cb8fe7836ec146a4fa44a97120fd6)
    (cherry picked from commit d91d76f35f972916ec61641113475a04fea31578)
    (cherry picked from commit 8a6a7b6d7fc545f8acbbc29c37f3a742d8c41e06)
    (cherry picked from commit 6e72705f055bc31cba1a170b7e7e4dadb6b64504)
    (cherry picked from commit c69f093f8dba41300c76ce6ceeafc9140728b528)
    (cherry picked from commit 645ddc4304c0e24822de289963fa91a98a95fa17)
    (cherry picked from commit 80fd6b23e82813f1fc7d4cd11646d4542e4e3090)

--- codex-rs/rollout/src/recorder.rs
+++ codex-rs/rollout/src/recorder.rs
@@ -95,6 +95,7 @@ pub enum RolloutRecorderParams {
     Create {
         session_id: SessionId,
         conversation_id: ThreadId,
+        wire_session_id: ThreadId,
         /// Overrides the rollout ID encoded in the filename.
         ///
         /// Normally this is `None`, so the filename is
@@ -187,6 +188,7 @@ impl RolloutRecorderParams {
     #[allow(clippy::too_many_arguments)]
     pub fn new(
         conversation_id: ThreadId,
+        wire_session_id: ThreadId,
         forked_from_id: Option<ThreadId>,
         parent_thread_id: Option<ThreadId>,
         source: SessionSource,
@@ -198,6 +200,7 @@ impl RolloutRecorderParams {
         Self::Create {
             session_id: conversation_id.into(),
             conversation_id,
+            wire_session_id,
             rollout_id_override: None,
             forked_from_id,
             forked_from_ordinal_exclusive: None,
@@ -842,6 +845,7 @@ impl RolloutRecorder {
             RolloutRecorderParams::Create {
                 session_id,
                 conversation_id,
+                wire_session_id,
                 rollout_id_override,
                 forked_from_id,
                 forked_from_ordinal_exclusive,
@@ -874,6 +878,7 @@ impl RolloutRecorder {
                 let session_meta = SessionMeta {
                     session_id,
                     id: conversation_id,
+                    wire_session_id: Some(wire_session_id),
                     forked_from_id,
                     forked_from_ordinal_exclusive: forked_from_ordinal_exclusive
                         .filter(|_| forked_from_id.is_some()),
